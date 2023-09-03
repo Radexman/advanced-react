4a159be89e8599cc8d7c3fc5121b8bffe0f57596
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-09-03 15:57:22 +0200

    Error Example

--- src/tutorial/01-useState-error-example.jsx
+++ src/tutorial/01-useState-error-example.jsx
@@ -1,4 +1,18 @@
-const UseState = () => {
-	return <div>useState</div>;
+const ErrorExample = () => {
+	let count = 0;
+
+	const increaseCount = () => {
+		count++;
+		console.log(count);
+	};
+
+	return (
+		<div>
+			<h2>{count}</h2>
+			<button type='button' className='btn' onClick={increaseCount}>
+				Increment
+			</button>
+		</div>
+	);
 };
-export default UseState;
+export default ErrorExample;
