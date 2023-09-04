a0669389b2179e48e02a4c117a49fc08f5e430f0
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-09-04 13:01:51 +0200

    Challange two & three

--- src/tutorial/useState/03-useState-array.jsx
+++ src/tutorial/useState/03-useState-array.jsx
@@ -4,6 +4,15 @@ import { data } from '../../data';
 const UseStateArray = () => {
 	const [people, setPeople] = useState(data);
 
+	const removeItem = (id) => {
+		const newPeople = people.filter((person) => person.id !== id);
+		setPeople(newPeople);
+	};
+
+	const clearAll = () => {
+		setPeople([]);
+	};
+
 	return (
 		<div>
 			{people.map((person) => {
@@ -11,9 +20,24 @@ const UseStateArray = () => {
 				return (
 					<div key={id}>
 						<h4>{name}</h4>
+						<button
+							type='button'
+							className='btn'
+							onClick={() => removeItem(id)}
+						>
+							Remove
+						</button>
 					</div>
 				);
 			})}
+			<button
+				type='button'
+				className='btn'
+				style={{ marginTop: '3rem', width: '50%' }}
+				onClick={clearAll}
+			>
+				Clear All
+			</button>
 		</div>
 	);
 };
