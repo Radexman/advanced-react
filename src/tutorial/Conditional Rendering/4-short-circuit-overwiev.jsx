10f91ad6ebc478e2e8d66f89ef492be6dabf8fe6
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-09-07 15:33:17 +0200

    Short Circuit Operator

--- src/tutorial/Conditional Rendering/4-short-circuit-overwiev.jsx
+++ src/tutorial/Conditional Rendering/4-short-circuit-overwiev.jsx
@@ -6,12 +6,15 @@ const ShortCircuit = () => {
 	// truthy
 	const [name, setName] = useState('Susan');
 
+	const codeExample = text || 'hello world';
+
 	return (
 		<div>
 			<h1>Falsy OR: {text || 'hello world'}</h1>
 			<h1>Falsy AND: {text && 'hello world'}</h1>
 			<h1>Truthy OR: {name || 'hello world'}</h1>
 			<h1>Truthy AND: {name && 'hello world'}</h1>
+			{codeExample}
 		</div>
 	);
 };
